169d715326973cbc3f75d5b4b1fdfecafca3cac7
helenaoliveira152 <helena_oliveira152@estudante.seisenai.org.br>
2026-09-10 15:31:05 -0300

    finalização

--- 00-demo/src/App.jsx
+++ 00-demo/src/App.jsx
@@ -5,17 +5,15 @@ function App() {
   return (
     <div className="cont-app">
       <h1>Trabalhando com React</h1>
-      <h2>Onde posso trabalhar como dev:</h2>
+      <h2>Onde posso trabalhar como dev?:</h2>
 
-      <p>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</p>
-
-      <p>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</p>
-
-      <p>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.</p>
-
-      <p>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</p>
-
-      <p>Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.</p>
+      <ul>
+        <li>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</li>
+        <li>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</li>
+        <li>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.</li>
+        <li>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</li>
+        <li>Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.</li>
+      </ul>
 
       <h2>Trabalhos com tecnologia mas sem programação:</h2>
 
@@ -27,6 +25,22 @@ function App() {
         <li>Gestão de Projetos e Agilidade</li>
         <li>Dados e Inteligência de Negócio</li>
         <li>Vendas Técnicas e Sucesso do Cliente</li>
+      </ul>
+
+      <h3>Vou fazer faculdade de outra coisa, ser DEV vai me ajudar? </h3>
+
+      <p>Se eu fizer faculdade de qualquer outra área que não seja TI/dev, como posso aproveitar minhas capacidades de dev nesse novo mundo?</p>
+
+
+     <div className='par'>
+      <p> <p>Aqui está como você pode aplicar suas habilidades de programação em diferentes áreas do conhecimento:</p></p>
+     </div>
+
+      <ul>
+        <li>Automação e Produtividade Pessoal</li>
+        <li>Análise de Dados Avançada e Pesquisa Científica</li>
+        <li>Empreendedorismo e Soluções Setoriais (HealthTech, LegalTech, EdTech)</li>
+        <li>Engenharia de Prompt e Inteligência Artificial</li>
       </ul>
 
     </div>
